5833399ae894421ff5ffb505d7e684df5286f4a8
QeaML <44324331+QeaML@users.noreply.github.com>
2020-01-29 19:06:53 +0100

    PHP

--- distancecalc.php
+++ distancecalc.php
@@ -1,3 +1,4 @@
+<?php
 function distance2d($from, $to){
     $diffX = abs($from[0] - $to[0]);
     $diffY = abs($from[1] - $to[1]);
@@ -17,3 +18,4 @@ function distance3d($from, $to){
 $a = [1.0, 2.0, 3.0];
 $b = [4.0, 5.0, 6.0];
 echo distance3d($a, $b);
+?>
