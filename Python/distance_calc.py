8f35b7c1e936ae0262ee705cee3aa593d24b4d8e
qeaml <qeaml@wp.pl>
2020-02-07 19:25:55 +0000

    Renamed variables and added return variable to Python.

--- Python/distance_calc.py
+++ Python/distance_calc.py
@@ -1,23 +1,25 @@
 from math import sqrt
 
-def distance2d(a, b):
-	diffX = abs(a[0] - b[0])
-	diffY = abs(a[1] - b[1])
+def distance2d(_from, to):
+	diffX = abs(_from[0] - to[0])
+	diffY = abs(_from[1] - to[1])
 	delta = sqrt((diffX ** 2) + (diffY ** 2))
 	return delta
 	
-def distance3d(a, b):
-	a2d = (a[0], a[2])
-	b2d = (b[0], b[2])
-	diffX = distance2d(a2d, b2d)
-	diffY = abs(a[1] - b[1])
+def distance3d(_from, to):
+	from2d = (_from[0], _from[2])
+	to2d = (to[0], to[2])
+	diffX = distance2d(from2d, to2d)
+	diffY = abs(_from[1] - to[1])
 	delta = sqrt((diffX ** 2) + (diffY ** 2))
 	return delta
 	
 if __name__ == "__main__":
-	print(
-		distance3d(
-			(1, 2, 3),
-			(4, 5, 6)
-		)
-	)
+	a = (1, 2, 3)
+	b = (4, 5, 6)
+	distance = distance3d(a, b)
+	print(distance)
+	if distance > 5.0:
+		exit(0)
+	else:
+		exit(1)
